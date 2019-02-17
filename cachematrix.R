84ad3abb9942489e5648a86aa17c1c4a7a847722
mtack88 <47107341+mtack88@users.noreply.github.com>
2019-02-17 08:27:00 +0100

    Function completed
    Insert function on empty file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function set the value of the matrix, get the value of the matrix,
+## set the inverse and then cache it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
+      list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse of the matrix, if has already been calculated return
+## the value from the cache, if not will recompute it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      m <- x$getinverse()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)
+      m
 }
